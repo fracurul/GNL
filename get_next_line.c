aed5094c72878122a547e0730eaab01ca9c350ea
Franco Stefano Curulla Galan <fracurul@c3r10s5.42malaga.com>
2023-12-03 19:02:26 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:05:55 by fracurul          #+#    #+#             */
-/*   Updated: 2023/11/25 19:17:46 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:56:37 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ char	*ft_get_line(char *s)
 	char	*line;
 	int		i;
 
-	i = ft_strchrgnl(s, '\n');
-	if (i == 0)
+	if (ft_strchrgnl(s, '\n'))
+		i = ft_strchrgnl(s, '\n');
+	else
 		i = ft_strlengnl(s);
 	line = (char *)ft_callocgnl(i + 1, 1);
 	if (!line)
@@ -31,10 +32,6 @@ char	*ft_get_line(char *s)
 	}
 	if (s[i] == '\n')
 		line[i] = '\n';
-	free(s);
-	i = 0;
-	if (line[i] == '\0')
-		return (free(line), NULL);
 	return (line);
 }
 
@@ -46,18 +43,42 @@ char	*ft_readgnl(int fd, char *s)
 	checkread = 1;
 	bufferword = (char *)ft_callocgnl(BUFFER_SIZE + 1, 1);
 	while (!bufferword)
-		return (free(bufferword), bufferword);
-	while (checkread > 0)
+		return (NULL);
+	while (checkread > 0 || ft_strchrgnl(s, '\n'))
 	{
 		checkread = read(fd, bufferword, BUFFER_SIZE);
 		if (checkread == -1)
 			return (free(bufferword), NULL);
-		s = ft_gnljoin(s, bufferword);
-		if (ft_strchrgnl(s, '\n') || checkread == 0)
+		if (checkread == 0)
 			break ;
+		s = ft_gnljoin(s, bufferword);
 	}
 	free(bufferword);
 	return (s);
+}
+
+char	*ft_buffer_ud(char *s)
+{
+	char	*excessbuffer;
+	int		i;
+	int		j;
+
+	if	(!s || !ft_strchrgnl(s, '\n'))
+		return (free (s), NULL);
+	i = ft_strchrgnl(s, '\n');
+	j = ft_strlengnl(s) - i;
+	excessbuffer = (char *)ft_callocgnl(j + 2, 1);
+	if (!excessbuffer)
+		return (free(s), NULL);
+	j = 0;
+	i++;
+	while (s && s[i + j])
+	{
+		excessbuffer[j] = s[i + j];
+		j++;
+	}
+	free(s);
+	return (excessbuffer);
 }
 
 char	*get_next_line(int fd)
@@ -65,11 +86,12 @@ char	*get_next_line(int fd)
 	static char	*s;
 	char		*aux;
 
-	s = NULL;
 	aux = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, s, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	s = ft_readgnl(fd, s);
+	if (s == NULL)
+		return (NULL);
 	aux = ft_get_line(s);
 	s = ft_buffer_ud(s);
 	return (aux);
