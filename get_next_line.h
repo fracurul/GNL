948e370842ffdbc91d409acdacd17ccc8f6984f7
Franco Stefano Curulla Galan <fracurul@c3r10s3.42malaga.com>
2023-12-16 20:20:53 +0100

    library

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:08:11 by fracurul          #+#    #+#             */
-/*   Updated: 2023/12/16 14:59:42 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/12/16 19:52:45 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 #  define BUFFER_SIZE 1000
 # endif
 
-void	*gnl_calloc(size_t count);
+void	*gnl_calloc(size_t count, size_t size);
 char	*get_next_line(int fd);
 char	*gnl_strjoin(char *s1, char *s2);
 char	*ft_buffer_ud(char *s);
 char	*ft_get_line(char *s);
-int		gnl_strchr(char *s, char c);
+int		gnl_strchr(char *s, int c);
 int		gnl_strlen(const char *s);
 
 #endif
