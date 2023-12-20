1fff9e3f8a9a95c457e0926ae9f544850bf48e2e
Franco Stefano Curulla Galan <fracurul@c3r8s3.42malaga.com>
2023-12-20 20:32:58 +0100

    utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 13:09:07 by fracurul          #+#    #+#             */
-/*   Updated: 2023/12/16 20:11:54 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/12/20 20:29:43 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,13 @@ char	*gnl_strjoin(char *s1, char *s2)
 		return (NULL);
 	size1 = gnl_strlen(s1);
 	size2 = gnl_strlen(s2);
-	str = (char *)gnl_calloc((size1 + size2) + 2, sizeof(char));
+	str = (char *)gnl_calloc(((size1 + size2) + 1), sizeof(char));
 	if (!str)
-		return (free(s1), NULL);
+		return (NULL);
 	i = -1;
 	while (++i < size1)
 		str[i] = s1[i];
+	i++;
 	i = -1;
 	while (++i < size2)
 		str[i + size1] = s2[i];
